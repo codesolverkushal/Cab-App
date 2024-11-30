7131ef3cbcf43138f1cc704c563cee79cbc17e5a
kushal <mohitsinghkpsc@gmail.com>
2024-11-30 23:18:09 +0530

    Profile and logout api added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {body} = require('express-validator');
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -14,5 +15,10 @@ router.post('/login',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({min:6}).withMessage('Password must be 6 length')
 ],userController.loginUser);
+
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile);
+
+router.get('/logout',authMiddleware.authUser,userController.logoutUser);
 
 module.exports = router;
