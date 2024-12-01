17aacf8dac17d2257d64c8ed0b7be0e1d35ea9a5
kushal <mohitsinghkpsc@gmail.com>
2024-12-01 23:03:47 +0530

    some correction in the frontend code

--- client/src/pages/CaptainSingnup.jsx
+++ client/src/pages/CaptainSingnup.jsx
@@ -11,7 +11,7 @@ const CaptainSingnup = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     setUserData({
-      username: {
+      fullname: {
         firstname: firstname,
         lastname: lastname,
       },
--- client/src/pages/UserSignup.jsx
+++ client/src/pages/UserSignup.jsx
@@ -11,7 +11,7 @@ const UserSignup = () => {
   const submitHandler = (e)=>{
     e.preventDefault();
     setUserData({
-      username:{
+      fullname:{
         firstname: firstname,
         lastname: lastname
       },
